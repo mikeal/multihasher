f5099c88b6e49b3e280bac25b1b8455de88dbdfa
Mikeal Rogers <mikeal.rogers@gmail.com>
2017-11-29 16:10:43 -0800

    feat(stream): Add stream support.

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const multihash = require('multihashes')
 const crypto = require('crypto')
+const isStream = require('is-stream')
 
 const algoMap = {sha256: 'sha2-256'}
 
 module.exports = (algo) => {
-  return async buffer => {
-    let hash = crypto.createHash(algo).update(buffer).digest()
-    let multi = multihash.encode(hash, algoMap[algo] || algo)
-    return multihash.toB58String(multi)
+  let _algo = algoMap[algo] || algo
+  return async value => {
+    if (Buffer.isBuffer(value)) {
+      let hash = crypto.createHash(algo).update(value).digest()
+      let multi = multihash.encode(hash, _algo)
+      return multihash.toB58String(multi)
+    } else if (isStream(value)) {
+      let _hasher = crypto.createHash(algo)
+      value.on('data', chunk => {
+        _hasher.update(chunk)
+      })
+      return new Promise((resolve, reject) => {
+        value.on('error', reject)
+        value.on('end', () => {
+          let multi = multihash.encode(_hasher.digest(), _algo)
+          resolve(multihash.toB58String(multi))
+        })
+      })
+    } else {
+      throw new Error('TypeError: must be buffer or stream')
+    }
   }
 }
--- test/test-hasher.js
+++ test/test-hasher.js
@@ -1,5 +1,6 @@
-let hasher = require('../')
-let test = require('tap').test
+const hasher = require('../')
+const bl = require('bl')
+const test = require('tap').test
 
 test('hasher: basic sha1', async t => {
   t.plan(1)
@@ -13,4 +14,24 @@ test('hasher: basic sha256', async t => {
   let _hasher = hasher('sha256')
   let hash = await _hasher(Buffer.from('asdf'))
   t.same(hash, 'QmeYzshSoNHr2QUWqmkMAy6raRhcmzTuroy7johWJNn3fY')
+})
+
+test('hasher: stream', async t => {
+  t.plan(1)
+  let _hasher = hasher('sha256')
+  let stream = bl()
+  stream.end(Buffer.from('asdf'))
+  let hash = await _hasher(stream)
+  t.same(hash, 'QmeYzshSoNHr2QUWqmkMAy6raRhcmzTuroy7johWJNn3fY')
+})
+
+test('invalid type', async t => {
+  t.plan(2)
+  let _hasher = hasher('sha256')
+  try {
+    await _hasher('asdf')
+  } catch (e) {
+    t.type(e, 'Error')
+    t.same(e.message, 'TypeError: must be buffer or stream')
+  }
 })
